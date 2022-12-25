511a5b7a8c3f3667920bdb2284933c6d64f79b28
“JieN” <624142800@qq.com>
2022-12-25 21:42:54 +0800

    redisson工具优化,未全部完成,待后续有缘人补充

--- src/main/java/com/iduck/sybin/toolkit/lock/redisson/autoconfiguration/RedissonAutoConfiguration.java
+++ src/main/java/com/iduck/sybin/toolkit/lock/redisson/autoconfiguration/RedissonAutoConfiguration.java
@@ -4,12 +4,19 @@ import cn.hutool.core.util.StrUtil;
 import com.iduck.sybin.toolkit.lock.redisson.properties.RedissonProperties;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
+import org.redisson.config.ReadMode;
 import org.redisson.config.SingleServerConfig;
+import org.redisson.connection.balancer.RandomLoadBalancer;
+import org.redisson.connection.balancer.RoundRobinLoadBalancer;
+import org.redisson.connection.balancer.WeightedRoundRobinBalancer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
 
 
 /**
@@ -28,7 +35,16 @@ public class RedissonAutoConfiguration {
         String model = redissonProperties.getModel();
         switch (model) {
             case "single":
-                config = createSingleConfig(redissonProperties);
+                createSingleConfig(config, redissonProperties);
+                break;
+            case "cluster":
+                createClusterConfig(config, redissonProperties);
+                break;
+            case "sentinel":
+                System.out.println("TODO:...sentinel...");
+                break;
+            case "master-salve":
+                System.out.println("TODO:...master-salve...");
                 break;
             default:
                 break;
@@ -36,8 +52,69 @@ public class RedissonAutoConfiguration {
         return Redisson.create(config);
     }
 
-    private Config createSingleConfig(RedissonProperties properties) {
-        Config config = new Config();
+    private void createClusterConfig(Config config, RedissonProperties properties) {
+        String[] nodeAddress = properties.getAddress().split(",");
+        for (int i = 0; i < nodeAddress.length; i++) {
+            nodeAddress[i] = RedissonProperties.ADDRESS_PREFIX + nodeAddress[i];
+        }
+
+        ClusterServersConfig clusterServersConfig = config.useClusterServers()
+                .addNodeAddress(nodeAddress);
+
+        // 集群状态扫描间隔时间,单位是毫秒
+        if (properties.getScanInterval() != null) {
+            clusterServersConfig.setScanInterval(properties.getScanInterval());
+        }
+
+        // 读取操作的负载均衡模式
+        // 枚举值为： salve（只在从服务节点里读取）、master（只在主服务节点里读取）、master_slave（在主从服务节点里都可以读取）
+        if (StrUtil.isNotEmpty(properties.getReadMode())) {
+            switch (properties.getReadMode()) {
+                case "master":
+                    clusterServersConfig.setReadMode(ReadMode.MASTER);
+                    break;
+                case "slave":
+                    clusterServersConfig.setReadMode(ReadMode.SLAVE);
+                    break;
+                case "master_slave":
+                    clusterServersConfig.setReadMode(ReadMode.MASTER_SLAVE);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // 负载均衡算法类的选择
+        if (StrUtil.isNotEmpty(properties.getLoadBalancer())) {
+            switch (properties.getLoadBalancer()) {
+                case "round":
+                    // 轮询调度算法（默认算法）
+                    clusterServersConfig.setLoadBalancer(new RoundRobinLoadBalancer());
+                    break;
+                case "weight":
+                    // 权重轮询调度算法（必须配置权重对）
+                    if (StrUtil.isNotEmpty(properties.getWeights()) && properties.getDefaultWeight() != null) {
+                        String[] weightPairs = properties.getWeights().split(",");
+                        HashMap<String, Integer> map = new HashMap<>();
+                        for (String weightPair : weightPairs) {
+                            String[] weight = weightPair.split("=");
+                            map.put(weight[0], Integer.parseInt(weight[1]));
+                        }
+                        clusterServersConfig.setLoadBalancer(new WeightedRoundRobinBalancer(map, properties.getDefaultWeight()));
+                    }
+                    break;
+                case "random":
+                    // 随机调度算法
+                    clusterServersConfig.setLoadBalancer(new RandomLoadBalancer());
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+
+    private void createSingleConfig(Config config, RedissonProperties properties) {
         SingleServerConfig singleServerConfig = config.useSingleServer()
                 .setAddress(RedissonProperties.ADDRESS_PREFIX + properties.getAddress());
         if (StrUtil.isNotEmpty(properties.getUsername()) && StrUtil.isNotEmpty(properties.getPassword())) {
@@ -47,6 +124,30 @@ public class RedissonAutoConfiguration {
         if (properties.getDatabase() != null) {
             singleServerConfig.setDatabase(properties.getDatabase());
         }
-        return config;
+
+        // 连接池最小空闲连接数
+        if (properties.getConnectionMinimumIdleSize() != null) {
+            singleServerConfig.setConnectionMinimumIdleSize(properties.getConnectionMinimumIdleSize());
+        }
+
+        // 连接池最大线程数
+        if (properties.getConnectionPoolSize() != null) {
+            singleServerConfig.setConnectionPoolSize(properties.getConnectionPoolSize());
+        }
+
+        // 线程超时时间
+        if (properties.getIdleConnectionTimeOut() != null) {
+            singleServerConfig.setIdleConnectionTimeout(properties.getIdleConnectionTimeOut());
+        }
+
+        // 客户端程序获取redis链接超时时间
+        if (properties.getConnectTimeout() != null) {
+            singleServerConfig.setConnectTimeout(properties.getConnectTimeout());
+        }
+
+        // 响应超时时间
+        if (properties.getTimeout() != null) {
+            singleServerConfig.setTimeout(properties.getTimeout());
+        }
     }
 }
--- src/main/java/com/iduck/sybin/toolkit/lock/redisson/properties/RedissonProperties.java
+++ src/main/java/com/iduck/sybin/toolkit/lock/redisson/properties/RedissonProperties.java
@@ -12,15 +12,82 @@ public class RedissonProperties {
 
     public static final String ADDRESS_PREFIX = "redis://";
 
+    /**
+     * 枚举类型（默认single）：single/master/sentinel/master-salve
+     */
     private String model = "single";
 
+    /**
+     * 链接地址（集群时可配置多个,使用英文,隔开）
+     */
     private String address = "127.0.0.1:6379";
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码
+     */
     private String password;
 
-    private Integer database;
+    /**
+     * 数据库编号
+     */
+    private Integer database = 0;
+
+    /**
+     * 连接池最小空闲连接数
+     */
+    private Integer connectionMinimumIdleSize;
+
+    /**
+     * 连接池最大线程数
+     */
+    private Integer connectionPoolSize;
+
+    /**
+     * 线程超时时间
+     */
+    private Integer idleConnectionTimeOut;
+
+    /**
+     * 客户端程序获取redis链接超时时间
+     */
+    private Integer connectTimeout;
+
+    /**
+     * 响应超时时间
+     */
+    private Integer timeout;
+
+    /**
+     * 集群状态扫描间隔时间,单位是毫秒
+     */
+    private Integer scanInterval;
+
+    /**
+     * 读取操作的负载均衡模式
+     * 枚举值为： salve（只在从服务节点里读取）、master（只在主服务节点里读取）、master_slave（在主从服务节点里都可以读取）
+     */
+    private String readMode;
+
+    /**
+     * 负载均衡算法类的选择
+     * 枚举值：round-轮询调度算法（默认算法）、weight-权重轮询调度算法（必须配置权重对）、random-随机调度算法
+     */
+    private String loadBalancer;
+
+    /**
+     * 权重键值对
+     */
+    private String weights;
+
+    /**
+     * 默认权重值
+     */
+    private Integer defaultWeight;
 
     public String getModel() {
         return model;
@@ -62,14 +129,104 @@ public class RedissonProperties {
         this.database = database;
     }
 
+    public Integer getConnectionMinimumIdleSize() {
+        return connectionMinimumIdleSize;
+    }
+
+    public void setConnectionMinimumIdleSize(Integer connectionMinimumIdleSize) {
+        this.connectionMinimumIdleSize = connectionMinimumIdleSize;
+    }
+
+    public Integer getConnectionPoolSize() {
+        return connectionPoolSize;
+    }
+
+    public void setConnectionPoolSize(Integer connectionPoolSize) {
+        this.connectionPoolSize = connectionPoolSize;
+    }
+
+    public Integer getIdleConnectionTimeOut() {
+        return idleConnectionTimeOut;
+    }
+
+    public void setIdleConnectionTimeOut(Integer idleConnectionTimeOut) {
+        this.idleConnectionTimeOut = idleConnectionTimeOut;
+    }
+
+    public Integer getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Integer getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(Integer timeout) {
+        this.timeout = timeout;
+    }
+
+    public Integer getScanInterval() {
+        return scanInterval;
+    }
+
+    public void setScanInterval(Integer scanInterval) {
+        this.scanInterval = scanInterval;
+    }
+
+    public String getReadMode() {
+        return readMode;
+    }
+
+    public void setReadMode(String readMode) {
+        this.readMode = readMode;
+    }
+
+    public String getLoadBalancer() {
+        return loadBalancer;
+    }
+
+    public void setLoadBalancer(String loadBalancer) {
+        this.loadBalancer = loadBalancer;
+    }
+
+    public String getWeights() {
+        return weights;
+    }
+
+    public void setWeights(String weights) {
+        this.weights = weights;
+    }
+
+    public Integer getDefaultWeight() {
+        return defaultWeight;
+    }
+
+    public void setDefaultWeight(Integer defaultWeight) {
+        this.defaultWeight = defaultWeight;
+    }
+
     @Override
     public String toString() {
         return "RedissonProperties{" +
-                ", model='" + model + '\'' +
+                "model='" + model + '\'' +
                 ", address='" + address + '\'' +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", database=" + database +
+                ", connectionMinimumIdleSize=" + connectionMinimumIdleSize +
+                ", connectionPoolSize=" + connectionPoolSize +
+                ", idleConnectionTimeOut=" + idleConnectionTimeOut +
+                ", connectTimeout=" + connectTimeout +
+                ", timeout=" + timeout +
+                ", scanInterval=" + scanInterval +
+                ", readMode='" + readMode + '\'' +
+                ", loadBalancer='" + loadBalancer + '\'' +
+                ", weights='" + weights + '\'' +
+                ", defaultWeight=" + defaultWeight +
                 '}';
     }
 }
